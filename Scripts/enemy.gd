57f7279042de8cd140e7fdda30e127dfb5c136d1
FelixWSC <159850732+FelixWSC@users.noreply.github.com>
2024-09-03 12:18:44 +1200

    Enemy and combat stuff

--- Scripts/enemy.gd
+++ Scripts/enemy.gd
@@ -4,8 +4,12 @@ var speed = 40
 var player_chase = false
 var player = null
 
+var health = 100
+var player_inattack_zone = false 
 
 func _physics_process(delta):
+	deal_with_damage()
+	
 	if player_chase:
 		position += (player.position - position)/speed 
 		
@@ -15,7 +19,7 @@ func _physics_process(delta):
 			$AnimatedSprite2D.flip_h = true
 		else:
 			$AnimatedSprite2D.flip_h = false
-			
+			 
 	else:
 		$AnimatedSprite2D.play("main_idle")
 
@@ -33,3 +37,22 @@ func _on_detection_area_body_exited(body):
 	player = null
 	player_chase = false
 	
+func enemy():
+	pass 
+
+
+func _on_enemy_hitbox_body_entered(body):
+	if body.has_method("player"):
+		player_inattack_zone = true  
+ 
+
+func _on_enemy_hitbox_body_exited(body):
+	if body.has_method("player"):
+		player_inattack_zone = false  
+		
+func deal_with_damage():
+	if player_inattack_zone and global.player_current_attack == true:
+		health = health - 20 
+		print("orc health = ", health )
+		if health <= 0:
+			self.queue_free()
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -5,6 +5,8 @@ var enemy_attack_cooldown = true
 var health = 100
 var player_alive = true 
 
+var attack_ip = false
+
 
 @export var speed = 100
 @export var acceleration = 0.1
@@ -30,6 +32,13 @@ func get_input() -> Vector2:
 
 func _physics_process(delta: float) -> void:
 	var direction = get_input()
+	enemy_attack()
+	
+	if health <= 0:
+		player_alive = false # You can add death or respawn screen here
+		health = 0
+		print("player has been killed")
+		self.queue_free() 
 	
 	# Set the animation based on input
 	if direction.length() > 0:
@@ -53,6 +62,8 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 
+func player():
+	pass
 
 func _on_player_hitbox_body_entered(body):
 	if body.has_method ("enemy"):
@@ -62,3 +73,14 @@ func _on_player_hitbox_body_entered(body):
 func _on_player_hitbox_body_exited(body):
 	if body.has_method ("enemy"):
 		enemy_inattack_range = false 
+
+func enemy_attack():
+	if enemy_inattack_range and enemy_attack_cooldown == true:
+		health = health - 20
+		enemy_attack_cooldown = false
+		$attack_cooldown.start()
+		print(health)
+
+
+func _on_attack_cooldown_timeout():
+	enemy_attack_cooldown = true
